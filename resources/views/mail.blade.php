8f0a14a0e5469a94874933e2626f73a307ff3f46
vinvintjg <vincent88tjong@gmail.com>
2023-03-23 14:10:41 +0700

    update new

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,5 +1,12 @@
-Dear me,
-
-trial mail oke
-
-Thanks,
+<p>Dear [Recipient's Name],</p>
+<br>
+<p>I hope this email finds you well. I am writing to you regarding.</p>
+<p>I would like to request [State your request clearly and specifically, if applicable].</p>
+<p>[Explain why this request is important, if necessary].</p>
+<p>Additionally, [Include any relevant details, if applicable].</p>
+<p>[Conclude your email with any closing remarks, if necessary].</p>
+<br>
+<p>Thank you for your time and attention to this matter.</p>
+<br>
+<p>Sincerely,</p>
+<p>[Your Name]</p>
